3bce55c3c94d6b4dfdfb7d2577c93f74acaa4c96
levindue <levin@duesterhus.net>
2025-06-14 23:42:03 +0200

    Problem 3

--- euler.hs
+++ euler.hs
@@ -16,3 +16,15 @@ evenFibs = 2 : 0 : zipWith (\x y -> 4 * y + x) evenFibs (tail evenFibs)
 
 problem2' :: Integer
 problem2' = sum $ takeWhile (< 4000000) evenFibs
+
+-- Problem 3: Largest Prime Factor
+
+problem3 :: Integer
+problem3 = factor 600_851_475_143 2
+  where
+    factor :: Integer -> Integer -> Integer
+    factor n candidate
+      | candidate * candidate > n =
+          if n > 1 then n else candidate
+      | n `mod` candidate == 0 = factor (n `div` candidate) candidate
+      | otherwise = factor n (candidate + 1)
